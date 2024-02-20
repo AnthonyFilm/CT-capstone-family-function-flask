00caece958b4875c8e2cbefb1a3e67dfc533fc97
Anthony <anthony@Anthonys-Mac-mini.local>
2024-02-20 17:40:18 -0500

    updated config

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config():
     REDIS_URI = os.environ.get("REDIS_URI")
     SESSION_TYPE = "redis"
     print(REDIS_URI)
-    SESSION_REDIS = redis.from_url("redis://default:16NT4bXOr6bNff0vRr8ftjvK4ZF3eNk6@redis-19920.c284.us-east1-2.gce.cloud.redislabs.com:19920")
+    SESSION_REDIS = redis.from_url(REDIS_URI)
 
     DREAMOBJECTS_SECRET_KEY=os.getenv('DREAMOBJECTS_SECRET_KEY')
     DREAMOBJECTS_KEY = os.getenv('DREAMOBJECTS_KEY')
